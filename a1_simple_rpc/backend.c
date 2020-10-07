7accff0647e7026f2dbd77ded2c52a5f01f032a1
eknuviad <edemnuviadenu@gmail.com>
2020-10-07 16:45:26 -0400

    Initial iteration without fully executable exit and shutdown command

--- a1_simple_rpc/backend.c
+++ a1_simple_rpc/backend.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "a1_lib.h"
 #include "rpc.h"
@@ -17,14 +19,7 @@ int multiplyInts(int a, int b){
 
 float divideFloats(float a, float b){
   return a/b;
-  //dont forget to report divide by zero error
 }
-
-//make the calculator sleep for x seconds - this is blocking
-// int sleep (int x){
-//  usleep(x);
-//   return 0;
-// }
 
 //return the factorial x
 uint16_t factorial(int x){
@@ -38,18 +33,16 @@ uint16_t factorial(int x){
 
 int main(int argc, char *argv[]) {
   int sockfd, clientfd;
-  // const char *greeting = "hello, world\n";
   char msg[BUFSIZE] = {0};
   char user_input[BUFSIZE] = {0};
-  const char *greeting;
   int running = 1;
+  int end_signal = 0;
   pid_t childpid;
 
   if (argc <= 2 || argc >3){
     fprintf(stderr,"Wrong number of arguments to setup server\n");
     return 0;
   }
-    // fgets(user_input, BUFSIZE, stdin);
 
   if (create_server(argv[1], atoi(argv[2]), &sockfd) < 0) {
     fprintf(stderr, "oh no\n");
@@ -70,7 +63,6 @@ int main(int argc, char *argv[]) {
     
       while (strcmp(msg, "quit\n")) {
         memset(msg, 0, sizeof(msg));
-        // char result[BUFSIZE];
         char result[BUFSIZE];
         char error[BUFSIZE];
         char cmd[BUFSIZE] = {0};
@@ -84,7 +76,6 @@ int main(int argc, char *argv[]) {
         char *token = strtok(msg, " ");
 
         if (strcmp(token, "add")==0){
-          //  printf("cmd is %s\n", token);
           //store inputs in char arrays
             while(token!=0){
               if (i == 1){
@@ -106,10 +97,53 @@ int main(int argc, char *argv[]) {
           sprintf(result, "%d", addInts(atoi(x),atoi(y)));
           printf("The final result is %s\n", result);
 
-        // }
-        // }else if (strcmp(token, "multiply\n")==0){
 
-        // }else if (strcmp(token,"divide\n")==0){
+        }else if (strcmp(token, "multiply")==0){
+          //store inputs in char arrays
+            while(token!=0){
+              if (i == 1){
+                strcpy(cmd,token);
+                printf("cmd is %s\n", cmd);
+              }else if (i == 2){
+                strcpy(x, token);
+                printf("x is %s\n", x);
+              }
+              else if(i == 3){
+              strcpy(y,token);
+                printf("y is %s\n", y);
+              }else{
+                printf("Illegal arguments");
+              }
+              token = strtok(0," ");
+              i++;
+          }
+          sprintf(result, "%d", multiplyInts(atoi(x),atoi(y)));
+          printf("The final result is %s\n", result);
+
+        }else if (strcmp(token,"divide")==0){
+              while(token!=0){
+              if (i == 1){
+                strcpy(cmd,token);
+                printf("cmd is %s\n", cmd);
+              }else if (i == 2){
+                strcpy(x, token);
+                printf("x is %s\n", x);
+              }
+              else if(i == 3){
+              strcpy(y,token);
+              printf("y is %s\n", y);
+              }else{
+                printf("Illegal arguments");
+              }
+              token = strtok(0," ");
+              i++;
+          }
+          if(strcmp(y,"0\n") == 0){
+            sprintf(result, "Error: Division by zero");
+          }else{
+          sprintf(result, "%f", divideFloats((float) atoi(x),(float) atoi(y)));
+          printf("The final result is %s\n", result);
+          }
 
         }else if (strcmp(token,"factorial")==0){
               while(token!=0){
@@ -121,7 +155,7 @@ int main(int argc, char *argv[]) {
                 printf("x is %s\n", x);
               }
             else{
-                printf("Illegal arguments"); // modify this to accurately capture error
+                printf("Illegal arguments");
               }
               token = strtok(0," ");
               i++;
@@ -129,28 +163,29 @@ int main(int argc, char *argv[]) {
           sprintf(result, "%d", factorial(atoi(x)));
           printf("The factorial result is %s\n", result);
 
-        // }else if (strcmp(token, "quit")==0){
 
         }else if (strcmp(token,"sleep")==0){
             token = strtok(0, " ");
-            //I need to send the message, sleep, send message
             sleep(atoi(token)); //make the calculator sleep for x seconds
           sprintf(result, "Done sleeping for %s seconds", token );
-          // printf("The factorial result is %s\n", result);
 
-        } else{
+        }else if (strcmp(token, "quit")==0 || strcmp(token, "shutdown")==0){
+          sprintf(result, "Goodbye! from Edem's server"); 
+          send_message(clientfd, result, BUFSIZE);
+          end_signal = 1; 
+          exit(0);
+        }
+        else{
           sprintf(result, "Error: Command %s not found", token);  
         }
         send_message(clientfd, result, BUFSIZE);
       }
     }
+
+    if(end_signal == 1){
+      break;
+    }
   }
 
   return 0;
 }
-
-//problem right now is that when i recieve the message from the client atm
-//it prints it out 3 times and the x y args give garbage values
-// a possible solutiopn to test out wil be to change the use of char
-//in the struct request
-//might need to find an efficient declaration of the struct with params istead
